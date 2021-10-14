5122b4bf1dbb7b7a377c160bac1e60c17a07e162
Ahmad Abutoimah <90200505+Ahmad-Abotoimah@users.noreply.github.com>
2021-10-14 18:59:35 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,36 @@
+let box = document.querySelector('.box');
+let preNum = 0;
+let opertype ='';
+let values = {prev:null , new:null};
+let clear = document.getElementById('c');
+
+function getnumbers(num){
+if (values.prev){
+    values.new = num;
+} else {
+    values.prev = num;
+}
+ box.value=num;
+}
+function getoperators(oper){
+    console.log(values)
+    opertype= oper;
+}
+function calc(){
+    if (!values.new)
+    return;
+    if(values.new&&values.prev&&opertype){
+    if (opertype==='+'){
+      box.value= values.prev+values.new
+    }else if (opertype==='-'){
+        box.value= values.prev-values.new
+    }else if (opertype==='*'){
+        box.value= values.prev*values.new
+    }else if (opertype==='/'){
+        box.value= values.prevvalues.new
+    }
+}
+}
+clear.onclick= function clearf(){
+    box.value= "";
+}
