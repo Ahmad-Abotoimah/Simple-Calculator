461b1c5bdca69b912f8a0fd4269f0adea8193a70
Ahmad Abutoimah <90200505+Ahmad-Abotoimah@users.noreply.github.com>
2021-10-14 20:05:03 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ let preNum = 0;
 let opertype ='';
 let values = {prev:null , new:null};
 let clear = document.getElementById('c');
-
 function getnumbers(num){
 if (values.prev){
     values.new = num;
@@ -17,20 +16,28 @@ function getoperators(oper){
     opertype= oper;
 }
 function calc(){
-    if (!values.new)
-    return;
+    if (!values.new){
+    return;}
     if(values.new&&values.prev&&opertype){
     if (opertype==='+'){
-      box.value= values.prev+values.new
+      let calculated=values.prev+values.new
+      box.value= calculated
+      values.prev=calculated
     }else if (opertype==='-'){
-        box.value= values.prev-values.new
+        let calculated=values.prev-values.new
+        box.value= calculated
+        values.prev=calculated
     }else if (opertype==='*'){
-        box.value= values.prev*values.new
+        let calculated=values.prev*values.new
+        box.value= calculated
+        values.prev=calculated
     }else if (opertype==='/'){
-        box.value= values.prev/values.new
-    }
-}
+        let calculated=values.prev/values.new
+        box.value= calculated
+        values.prev=calculated
+                      }
+             }
 }
 clear.onclick= function clearf(){
-    box.value= "";
+    box.value= "0";
 }
